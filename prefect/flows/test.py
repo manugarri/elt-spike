00c7e844143f6f3edf87bfdbf3563dbee60b1396
Manuel Garrido <manuel.garrido@carpe.io>
2022-11-16 12:58:19 +0000

    add param at the test flow level

--- prefect/flows/test.py
+++ prefect/flows/test.py
@@ -1,11 +1,16 @@
+"""
+Sample flow.
+
+"""
 from prefect import task, flow, get_run_logger
 
-VERSION = "1.0.0"
+FLOW_NAME = "test"
+FLOW_VERSION = "1.0.0"
 
 import os
 
 @task
-def list_files(logger, startpath="."):
+def list_files(logger, startpath):
     for root, dirs, files in os.walk(startpath):
         level = root.replace(startpath, '').count(os.sep)
         indent = ' ' * 4 * (level)
@@ -14,11 +19,12 @@ def list_files(logger, startpath="."):
         for f in files:
             logger.info('{}{}'.format(subindent, f))
 
-@flow(name="test", version=VERSION)
-def test():
+@flow(name=FLOW_NAME, version=FLOW_VERSION)
+def test(startpath="."):
     logger = get_run_logger()
-    list_files(logger)
-    logger.info(f"OK. VERSION: {VERSION}")
+    logger.debug(f"STARTING FLOW {FLOW_NAME}.{FLOW_VERSION}")
+    logger.info(f"LOGGING FILES IN PATH '{startpath}'")
+    list_files(logger, startpath)
     return True
     
 
